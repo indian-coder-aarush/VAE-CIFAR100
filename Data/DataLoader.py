11143e6e05102fd845e48fa583d86b9ebe462d07
Aarush Shirpewar <aarushrshirpewar@gmail.com>
2026-01-20 23:02:43 +0530

    did some image analysis

--- Data/DataLoader.py
+++ Data/DataLoader.py
@@ -2,17 +2,20 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 import pickle
 import numpy as np
+import os
+
+BASE_DIR = os.path.dirname(__file__)
 
 def load_data(path):
+    path = os.path.join(BASE_DIR, path)
     with open(path, 'rb') as f:
-        data = pickle.load(f)
+        data = pickle.load(f, encoding = 'latin1')
     return data
 
-train = load_data('train')['data']
-test = load_data('test')['data']
-
-train.reshape(-1,3,32,32)
-test.reshape(-1.3,32,32)
+def load_all_data():
+    train = load_data('train')['data'].reshape(-1,3,32,32)
+    test = load_data('test')['data'].reshape(-1,3,32,32)
+    return train, test
 
 class ImageDataset(Dataset):
 
@@ -22,12 +25,11 @@ class ImageDataset(Dataset):
 
     def __getitem__(self, index):
         img = self.data[index]
+        return img
 
     def __len__(self):
         return self.length
 
-train_dataset = ImageDataset(train)
-test_dataset = ImageDataset(test)
-
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers = 2)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True, num_workers = 2)
+def make_dataloader(data):
+    dataset = ImageDataset(data)
+    return DataLoader(dataset, batch_size = 32, shuffle = True, num_workers = 2)
